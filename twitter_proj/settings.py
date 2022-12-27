6babe1ff65b48e758b25afde510a826d21c598fa
NessaAz <vanessa.azenwa@gmail.com>
2022-12-27 11:57:09 +0300

    add environment vars, create views

--- twitter_proj/settings.py
+++ twitter_proj/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g3+8cjco-bs*3j81^pv!k(@ng##*#o3hj=o+vt#39_ss2)r$p9'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
